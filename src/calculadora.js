b7c009f5bfefc94e54fa90909bf3cd2a7f97d031
Ana Luiza <analuiza_04@hotmail.com>
2025-08-28 23:12:50 -0300

    Add soma e teste

---
+++ src/calculadora.js
@@ -0,0 +1,19 @@
+function realizarSomaDoisCamposDaTela() {
+    const campo1 = document.getElementById('campo1').value;
+    const campo2 = document.getElementById('campo2').value;     
+    const resultado = somarDoisNumeros(campo1 + campo2) 
+}
+
+function somarDoisNumeros(numero1, numero2) {
+    const resultado = (numero1) + (numero2);
+    return resultado
+}
+
+function calcularMediaDoisNumeros(numero1, numero2) {
+    const resultado = (numero1 + numero2) / 2;
+    return resultado;
+}
+
+module.exports = {
+    somarDoisNumeros
+};
---
+++ tests/calculadora.test.js
@@ -0,0 +1,5 @@
+const {somarDoisNumeros} = require('../src/calculadora');
+
+const resultadoDaSoma = somarDoisNumeros(2, 3);
+
+console.log('Resultado da soma: ', resultadoDaSoma);
