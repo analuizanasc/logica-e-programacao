4fd3c0305b5ada073bef431a6759a5a28a82b606
Ana Luiza <analuiza_04@hotmail.com>
2025-09-04 08:52:18 -0300

    Adição de demais funções e testes da calculadora

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,11 +1,20 @@
-function realizarSomaDoisCamposDaTela() {
-    const campo1 = document.getElementById('campo1').value;
-    const campo2 = document.getElementById('campo2').value;     
-    const resultado = somarDoisNumeros(campo1 + campo2) 
-}
-
 function somarDoisNumeros(numero1, numero2) {
     const resultado = (numero1) + (numero2);
+    return resultado
+}
+
+function multiplicarDoisNumeros(numero1, numero2) {
+    const resultado = (numero1) * (numero2);
+    return resultado
+}
+
+function dividirDoisNumeros(numero1, numero2) {
+    const resultado = (numero1) / (numero2);
+    return resultado
+}
+
+function subtrairDoisNumeros(numero1, numero2) {
+    const resultado = (numero1) - (numero2);
     return resultado
 }
 
--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -15,4 +15,94 @@ describe('Testes da função Soma', function () {
         // compara o resultado 
         expect(resultadoSoma).to.equal(-5);
     });
+
+    it('A função deve ser capaz de somar dois números negativos', function () {
+        // coleta o resultado 
+        const resultadoSoma = somarDoisNumeros(-5, -10);
+        // compara o resultado 
+        expect(resultadoSoma).to.equal(-15);
+    });
+
+    it('A função deve ser capaz de somar zeros', function () {
+        // coleta o resultado 
+        const resultadoSoma = somarDoisNumeros(0, 0);
+        // compara o resultado 
+        expect(resultadoSoma).to.equal(0);
+    });
+});
+
+describe('Testes da função de Subtração', function () {
+    it('A função deve ser capaz de subtrair valor 1 maior que valor 2', function () {
+        // coleta o resultado 
+        const resultado = subtrairDoisNumeros(10, 5);
+        // compara o resultado 
+        expect(resultado).to.equal(5);
+    });
+    it('A função deve ser capaz de subtrair valor 1 menor que valor 2', function () {
+        // coleta o resultado 
+        const resultado = subtrairDoisNumeros(5, 15);
+        // compara o resultado 
+        expect(resultado).to.equal(-10);
+    });
+    it('A função deve ser capaz de subtrair dois numeros negativos', function () {
+        // coleta o resultado 
+        const resultado = subtrairDoisNumeros(-5, -15);
+        // compara o resultado 
+        expect(resultado).to.equal(-20);
+    });
+});
+
+
+describe('Testes da função de Multiplicação', function () {
+    it('A função deve ser capaz de multiplicar 2 números positivos', function () {
+        // coleta o resultado 
+        const resultado = multiplicarDoisNumeros(10, 5);
+        // compara o resultado 
+        expect(resultado).to.equal(50);
+    });
+    it('A função deve ser capaz de multiplicar 2 números negativos', function () {
+        // coleta o resultado 
+        const resultado = multiplicarDoisNumeros(-5, -1);
+        // compara o resultado 
+        expect(resultado).to.equal(5);
+    });
+    it('A função deve ser capaz de multiplicar por 1 número positivo e 1 número negativo', function () {
+        // coleta o resultado 
+        const resultado = multiplicarDoisNumeros(-5, 1);
+        // compara o resultado 
+        expect(resultado).to.equal(-5);
+    });
+    it('A função deve ser capaz de multiplicar por 1 número por zero ', function () {
+        // coleta o resultado 
+        const resultado = multiplicarDoisNumeros(5, 0);
+        // compara o resultado 
+        expect(resultado).to.equal(0);
+    });
+});
+
+describe('Testes da função de Divisão', function () {
+    it('A função deve ser capaz de dividir 2 números, valor 01 maior que valor 02', function () {
+        // coleta o resultado 
+        const resultado = dividirDoisNumeros(10, 5);
+        // compara o resultado 
+        expect(resultado).to.equal(2);
+    });
+    it('A função deve ser capaz de dividir 2 números, valor 01 menor que valor 02', function () {
+        // coleta o resultado 
+        const resultado = dividirDoisNumeros(5, 10);
+        // compara o resultado 
+        expect(resultado).to.equal(0.5);
+    });
+    it('A função deve ser capaz de dividir um número por outro negativo', function () {
+        // coleta o resultado 
+        const resultado = dividirDoisNumeros(-5, 1);
+        // compara o resultado 
+        expect(resultado).to.equal(-5);
+    });
+    it('A função deve ser capaz de dividir 1 número por zero ', function () {
+        // coleta o resultado 
+        const resultado = dividirDoisNumeros(5, 0);
+        // compara o resultado 
+        expect(resultado).to.equal(1);
+    });
 });
